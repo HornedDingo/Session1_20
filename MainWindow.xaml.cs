2a51042e572cc2fb31bfc5c9018f5dd48bf9eb72
С.Ю <isip_s.yu.samsonova@mpt.ru>
2023-03-01 16:50:30 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session1_20
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            var uri = new Uri("light.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Exit1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
